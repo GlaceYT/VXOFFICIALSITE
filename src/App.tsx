b6cc290f40c42cd4aacb4723a2dc10a5328d72be
GlaceYT <96680732+GlaceYT@users.noreply.github.com>
2024-12-13 19:10:34 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import Documentation from './pages/Documentation'
-import Bot from './pages/bot'
+import Server from './pages/Server'
 import Feedbacks from './pages/Feedbacks'
 import Vouches from './pages/Vouches'
 function App() {
@@ -16,7 +16,7 @@ function App() {
         <Route path="/documentation" element={<Documentation />} />
         <Route path="/vouches" element={<Vouches />} />
         <Route path="/feedbacks" element={<Feedbacks />} />
-        <Route path="/bot" element={<Bot />} />
+        <Route path="/server" element={<Server />} />
       </Routes>
     </Router>
   )
